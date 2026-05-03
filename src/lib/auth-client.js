888fed97b82007e646fb827c83de6eb51e7ec62e
muradvcv <muradvcv@gmail.com>
2026-05-03 12:03:57 +0600

    proxy file added

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,16 @@
-import { createAuthClient } from "better-auth/react"
+// import { createAuthClient } from "better-auth/react"
+// export const authClient = createAuthClient({
+//   /** The base URL of the server (optional if you're using the same domain) */
+//   baseURL:"https://tiles-gallery-zeta-one.vercel.app"
+// })
+// export const { signIn, signUp, useSession } = createAuthClient()
+
+
+import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL:"https://tiles-gallery-zeta-one.vercel.app"
-})
-export const { signIn, signUp, useSession } = createAuthClient()
+  baseURL: process.env.BETTER_AUTH_URL, 
+});
+
+
+export const { signIn, signUp, useSession } = authClient;
---
+++ src/proxy.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+
+export async function proxy(request) {
+
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  // console.log(session,'session');
+
+  // const isLogin=false;
+  if (session) {
+
+    return NextResponse.next()
+
+  }
+
+
+  return NextResponse.redirect(new URL('/login', request.url))
+}
+
+export const config = {
+  matcher: ['/my-profile','/all-tiles/:path*'],
+}
